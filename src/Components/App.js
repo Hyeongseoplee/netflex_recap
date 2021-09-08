77d82a60152555fa9152e75e1f0eb93d9fcefc85
vennylee <hyungsup42134@gmail.com>
2021-09-08 16:46:49 +0900

    feat: Create a header - 링크 연결, 스타일
    Add a globalstyle

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,12 @@
 import Routers from './Routers';
+import GlobalStyle from './GlobalStyle';
+import styled from 'styled-components';
 
 export default function App() {
   return (
-      <Routers/>
+      <>
+        <Routers/>
+        <GlobalStyle/>
+      </>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,63 @@
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const SHeader = styled.header`
+padding-top:20px;
+border-bottom:2px solid black;
+box-shadow: 2px 2px 5px black;
+`;
+
+const Header_wrapper = styled.div`
+    padding-top: 8px;
+    padding-bottom: 10px;
+    display:flex;
+    margin:0 2.5rem;
+`;
+
+const Header_logo = styled.div`
+    color:red;
+    font-size:35px;
+`;
+
+const Links = styled.ul`
+    display: flex;
+    gap:15px;
+    align-items: center;
+    justify-content: space-between;
+    margin-left: 30px;
+`;
+
+const List = styled.li`
+    list-style : none;
+    text-align: center;
+    display:flex;
+    align-items: center;
+    padding-bottom:10px;
+    border-bottom: 5px solid lightgray;
+`;
+
+const SLink = styled(Link)`
+    text-decoration-line : none;
+    padding: 2px 10px;
+`;
 
 export default function Header() {
     return (
-        <h4>header</h4>
+        <SHeader>
+            <Header_wrapper>
+            <Header_logo>Netflix</Header_logo>
+            <Links>
+                <List>
+                    <SLink to="/">Home</SLink>
+                </List>
+                <List>
+                    <SLink to="/tv">TV</SLink>
+                </List>                
+                <List>
+                    <SLink to="/search">Search</SLink>
+                </List>
+            </Links>
+            </Header_wrapper>
+        </SHeader>
     )
 }
