677f0d8c3db0892d14c5edc983426d24e9060058
vennylee <hyungsup42134@gmail.com>
2021-09-09 23:13:16 +0900

    feat: Fetch data when we enter term into search bar(not finished)

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,6 +1,32 @@
+import { useEffect, useState } from 'react';
 import SearchPresenter from './SearchPresenter';
+import { MovieApi } from '../../api';
+
 
 export default function SearchContainer() {
+    const [ result, setResult ] = useState(null);
+    const [ error, setError ] = useState(null);
+    const [ isLoading, setLoading ] = useState(false);
+    const term = "hello";
+
+    
+
+    useEffect(() => {
+        const fetchData = async() => {
+            try {
+                setLoading(true);
+                const { data : { results : movieSearch}} = await MovieApi.movieSearch(term);
+                setResult(movieSearch);
+            } catch {
+                setError("Failed");
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
+    },[])
+    console.log(result)
+
     return (
         <SearchPresenter/>
     )
