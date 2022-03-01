4237625abc9b678b785499f49556389a7a759c91
vennylee <hyungsup42134@gmail.com>
2022-03-01 15:45:58 +0900

    add homepage

--- src/Components/Routers.js
+++ src/Components/Routers.js
@@ -1,21 +1,21 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Header from './Header';
-import Home from '../Routes/Home';
-import TV from '../Routes/TV';
-import Seacrh from '../Routes/Search';
-import Detail from '../Routes/Detail';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./Header";
+import Home from "../Routes/Home";
+import TV from "../Routes/TV";
+import Seacrh from "../Routes/Search";
+import Detail from "../Routes/Detail";
 
 export default function Routers() {
-    return (
-        <Router>
-        <Header/>
-        <Switch>
-            <Route path="/home" component = { Home }></Route>
-            <Route path="/search" component={ Seacrh }></Route>
-            <Route path="/movie/:id" component= { Detail }></Route>
-            <Route path="/tv" exact component={ TV }></Route>
-            <Route path="/tv/:id" component= { Detail }></Route>
-        </Switch>
-        </Router>
-    )
-} 
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <Header />
+      <Switch>
+        <Route path="/home" component={Home}></Route>
+        <Route path="/search" component={Seacrh}></Route>
+        <Route path="/movie/:id" component={Detail}></Route>
+        <Route path="/tv" exact component={TV}></Route>
+        <Route path="/tv/:id" component={Detail}></Route>
+      </Switch>
+    </Router>
+  );
+}
